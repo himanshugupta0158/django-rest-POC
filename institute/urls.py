392b415fe1b790206702865d9cf438ff1fcdfc06
himanshugupta0158 <himanshugupta0158@gmail.com>
2023-11-20 18:27:23 +0530

    logout problem resolved

--- institute/urls.py
+++ institute/urls.py
@@ -4,6 +4,6 @@ from .views import HomePageView, LoginView, SignUpView, LogoutView
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('login/', LoginView.as_view(), name="login"),
-    path('logout/', LoginView.as_view(), name="logout"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignUpView.as_view(), name="signup"),
 ]
